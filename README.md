f152ecc301baae796b161fa5af5b2e800c982436
Dj Walker-Morgan <codepope@gmail.com>
2016-09-22 11:29:51 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,79 @@
+# compose-postgresql-helloworld-nodejs overview
+
+compose-postgresql-helloworld-nodejs is a sample Bluemix application which shows you how to connect to an IBM Compose for PostgreSQL for Bluemix service using Node.js.
+
+## Running the app on Bluemix
+
+1. If you do not already have a Bluemix account, [sign up here][bluemix_signup_url]
+
+2. Download and install the [Cloud Foundry CLI][cloud_foundry_url] tool
+
+3. Clone the app to your local environment from your terminal using the following command:
+
+  ```
+  git clone https://github.com/IBM-Bluemix/compose-postgresql-helloworld-nodejs.git
+  ```
+
+4. `cd` into this newly created directory
+
+5. Open the `manifest.yml` file and change the `host` value to something unique.
+
+  The host you choose will determinate the subdomain of your application's URL:  `<host>.mybluemix.net`
+
+6. Connect to Bluemix in the command line tool and follow the prompts to log in.
+
+  ```
+  $ cf api https://api.ng.bluemix.net
+  $ cf login
+  ```
+
+7. Create the Compose for PostgreSQL service in Bluemix.
+
+  **Note :** The Compose for PostgreSQL service does not offer a free plan. For details of pricing, see the _Pricing Plans_ section of the [Compose for PostgreSQL service][compose_for_postgresql_url] in Bluemix.
+
+  ```
+  $ cf create-service compose-postgresql-helloworld-nodejs Standard my-compose-for-postgresql-service
+  ```
+
+8. Push the app to Bluemix.
+
+  ```
+  $ cf push
+  ```
+
+Now when you visit `<host>.mybluemix.net/` you will be able to view and add words to the _words_ table.
+
+## Code Structure
+
+| File | Description |
+| ---- | ----------- |
+|[**server.js**](server.js)|Establishes a connection to the PostgreSQL database using credentials from VCAP_ENV and handles create and read operations on the database. |
+|[**main.js**](public/javascripts/main.js)|Handles user input for a PUT command and parses the results of a GET command to output the contents of the PostgreSQL database.|
+
+The app uses a PUT and a GET operation:
+
+- PUT
+  - takes user input from [main.js](public/javascript/main.js)
+  - uses the `client.query` method to add the user input to the words table
+
+- GET
+  - uses `client.query` method to retrieve the contents of the _words_ table
+  - returns the response of the database command to [main.js](public/javascript/main.js)
+
+## Privacy Notice
+The sample web application includes code to track deployments to Bluemix and other Cloud Foundry platforms. The following information is sent to a [Deployment Tracker](https://github.com/cloudant-labs/deployment-tracker) service on each deployment:
+
+* Application Name (application_name)
+* Space ID (space_id)
+* Application Version (application_version)
+* Application URIs (application_uris)
+
+This data is collected from the VCAP_APPLICATION environment variable in IBM Bluemix and other Cloud Foundry platforms. This data is used by IBM to track metrics around deployments of sample applications to IBM Bluemix. Only deployments of sample applications that include code to ping the Deployment Tracker service will be tracked.
+
+### Disabling Deployment Tracking
+
+Deployment tracking can be disabled by removing `require("cf-deployment-tracker-client").track();` from the beginning of the `server.js` file.
+
+[compose_for_postgreSQL_url]: https://new-console.ng.bluemix.net/catalog/services/compose-for-postgreSQL/
+[bluemix_signup_url]: https://ibm.biz/compose-for-postgreSQL-signup
+[cloud_foundry_url]: https://github.com/cloudfoundry/cli
---
+++ server.js
@@ -0,0 +1,134 @@
+/**
+ * Copyright 2016 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the “License”);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an “AS IS” BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ // First add the obligatory web framework
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// Util is handy to have around, so thats why that's here.
+const util = require('util')
+// and so is assert
+const assert = require('assert');
+
+// We want to extract the port to publish our app on
+var port = process.env.VCAP_APP_PORT || 8080;
+
+// Then we'll pull in the database client library
+var pg = require('pg');
+
+// Now lets get cfenv and ask it to parse the environment variable
+var cfenv = require('cfenv');
+var appenv = cfenv.getAppEnv();
+
+// Within the application environment (appenv) there's a services object
+var services = appenv.services;
+
+// The services object is a map named by service so we extract the one for PostgreSQL
+var pg_services = services["compose-for-postgresql"];
+
+// This check ensures there is a services for MongoDB databases
+// assert(!util.isUndefined(pg_services), "Must be bound to compose-for-postgresql services");
+
+// We now take the first bound MongoDB service and extract it's credentials object
+var credentials = pg_services[0].credentials;
+
+// Within the credentials, an entry ca_certificate_base64 contains the SSL pinning key
+// We convert that from a string into a Buffer entry in an array which we use when
+// connecting.
+var ca = new Buffer(credentials.ca_certificate_base64, 'base64');
+var connectionString = credentials.uri;
+
+// We want to parse connectionString to get username, password, database name, server, port
+// So we can use those to connect to the database
+var parse = require('pg-connection-string').parse;
+config = parse(connectionString);
+
+// Add some ssl
+config.ssl = {
+  rejectUnauthorized: false,
+  ca: ca
+}
+
+// set up a new client using our config details
+var client = new pg.Client(config);
+
+client.connect(function(err) {
+  if (err) {
+   response.status(500).send(err);
+  } else {
+    client.query('CREATE TABLE words (word varchar(256) NOT NULL, definition varchar(256) NOT NULL)', function (err,result){
+      if (err) {
+        console.log(err)
+      }
+    });
+  }
+});
+
+// We can now set up our web server. First up we set it to serve static pages
+app.use(express.static(__dirname + '/public'));
+
+app.put("/words", function(request, response) {
+  // set up a new client using our config details
+  var client = new pg.Client(config);
+  client.connect(function(err) {
+    if (err) {
+     response.status(500).send(err);
+    } else {
+      var queryText = 'INSERT INTO words(word,definition) VALUES($1, $2)';
+
+      client.query(queryText, [request.body.word,request.body.definition], function (error,result){
+        if (error) {
+         response.status(500).send(error);
+        } else {
+         response.send(result);
+        }
+      });
+    }
+  });
+});
+
+// Read from the database when someone visits /hello
+app.get("/words", function(request, response) {
+  // set up a new client using our config details
+  var client = new pg.Client(config);
+  // connect to the database
+  client.connect(function(err) {
+
+    if (err) throw err;
+
+    // execute a query on our database
+    client.query('SELECT * FROM words ORDER BY word ASC', function (err, result) {
+      if (err) {
+       response.status(500).send(err);
+      } else {
+       response.send(result.rows);
+      }
+
+    });
+
+  });
+
+});
+
+// Now we go and listen for a connection.
+app.listen(port);
+
+require("cf-deployment-tracker-client").track();
